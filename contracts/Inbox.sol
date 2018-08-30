3bc9ed693cb6fe9404f4f52766db5a895d0ae4df
sonu <sonusaikishan@gmail.com>
2018-08-30 20:00:39 +0530

    Completed compile, deploy & test of contract on ganache local network

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract Inbox
+{
+    string public message;
+    
+    constructor(string initMessage) public
+    {
+        message = initMessage;
+    }
+    
+    function setMessage(string newMessage) public
+    {
+        message = newMessage;
+    }
+    
+}
